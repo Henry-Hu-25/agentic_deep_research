98b04a103794b17267727ea8e67dbc414427850e
Henry-Hu-25 <qhu16@dons.usfca.edu>
2025-08-26 00:24:12 -0700

    updated scoping

--- scoping.py
+++ scoping.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel, Field
 
 class ClarifyWithUser(BaseModel):
     '''
-    Schema for user clarification decision and questions
+    Schema for user clarification decision and questions.
     '''
 
     need_clarification: bool = Field(
@@ -17,14 +17,18 @@ class ClarifyWithUser(BaseModel):
     )
 
     question: str = Field(
-        description="A clarifying question to ask the user"
+        description="A clarifying question to ask the user."
     )
 
     verification: str = Field(
-        description="A verification message signaling that the research will start after the user has provided the necessary information"
+        description="A verification message signaling that the research will start after the user has provided the necessary information."
     )
 
 class ResearchQuestion(BaseModel):
     '''
-    
+    Schema for the final, refined research question.
     '''
+
+    research_brief: str = Field(
+        description ="The final, refined research question that will be used to guide the research."
+    )
